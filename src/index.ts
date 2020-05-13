4de9e51d9030efa078f4e663bc27bfdd7116e43d
Özüm Eldoğan <ozum@ozum.net>
2020-05-13 18:51:45 +0300

    fix: fef

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,6 @@
  * @returns number
  */
 export default function deneme(a: number): number {
-<<<<<<< HEAD
   const b = 7323;
-  return a; // 12344
-=======
-  // const b = 732;
   return a; // 12344f
->>>>>>> d690fdad6b8c9264beae4385394cb726be0e7c02
 }
